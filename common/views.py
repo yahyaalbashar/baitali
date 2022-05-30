7826f369d2193742f6baebcf1d8ad05826a095c7
yahyaalbashar <v.salvatore7.gs@gmail.com>
2022-05-30 16:03:35 +0300

    home page

--- common/views.py
+++ common/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "common/_base.html",{})
+    return render(request, "common/home.html",{})
