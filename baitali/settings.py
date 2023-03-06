1bcf84427c06959aba6527bcc65b9cfd18159c07
yahyaalbashar <v.salvatore7.gs@gmail.com>
2023-03-06 22:28:59 +0300

    Add allowed hosts

--- baitali/settings.py
+++ baitali/settings.py
@@ -134,3 +134,9 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "www.bait-ali.com",
+    "bait-ali.com",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
